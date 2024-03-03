47b7192f283135b03e0e4caaa318a18ce4572957
NOBITA_XD <156512147+nobitaaxd@users.noreply.github.com>
2024-03-03 11:33:39 +0530

    Update __init__.py

--- Japanese/__init__.py
+++ Japanese/__init__.py
@@ -20,7 +20,7 @@ app = Client(
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN, BOT_TOKEN2, BOT_TOKEN3, BOT_TOKEN4, BOT_TOKEN5, BOT_TOKEN6, BOT_TOKEN7, BOT_TOKEN8, BOT_TOKEN9, BOT_TOKEN10,
-    in_memory=True)
+    in_memory=True,)
 
 boot = time.time()
 async def eval_bot():
