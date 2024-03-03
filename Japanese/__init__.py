347f3bba681e0dfef3f6b7cae6a4ecf0e9b2fcc3
Sᴏυʀαʏєɴ <157188588+Sourayen@users.noreply.github.com>
2024-03-03 11:51:48 +0530

    Accha

--- Japanese/__init__.py
+++ Japanese/__init__.py
@@ -19,8 +19,7 @@ app = Client(
     "Eval",
     api_id=API_ID,
     api_hash=API_HASH,
-    bot_token=BOT_TOKEN, BOT_TOKEN2, BOT_TOKEN3, BOT_TOKEN4, BOT_TOKEN5, BOT_TOKEN6, BOT_TOKEN7, BOT_TOKEN8, BOT_TOKEN9, BOT_TOKEN10,
-    in_memory=True
+    bot_token=BOT_TOKEN, BOT_TOKEN2, BOT_TOKEN3, BOT_TOKEN4, BOT_TOKEN5, BOT_TOKEN6, BOT_TOKEN7, BOT_TOKEN8, BOT_TOKEN9, BOT_TOKEN10
 )
 
 boot = time.time()
